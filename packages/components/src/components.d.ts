1b47ef52d8bc979dfd02af68153c7af6d2db05dc
liruixaing <3054809485@qq.com>
2024-08-21 16:40:01 +0800

    init:初次提交

---
+++ packages/components/src/components.d.ts
@@ -0,0 +1,7 @@
+import * as components from "./index";
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+    ArButton: typeof components.Button;
+  }
+}
+export {};
---
+++ play/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import arim from "@arim-ui/components";
+
+createApp(App).use(arim).mount("#app");
