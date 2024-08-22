a4d55711b6f676d8cd1d1af7c858c81cb122c503
liruixaing <3054809485@qq.com>
2024-08-22 16:18:07 +0800

    chore(css): 🐳 使用gulp单独打包css

--- packages/components/script/build/index.ts
+++ packages/components/script/build/index.ts
@@ -1,10 +1,32 @@
 import delPath from "./delpaths";
-import { series, parallel } from "gulp";
-import { pkgPath } from "./paths";
+import { series, parallel, src, dest } from "gulp";
+import { componentPath, pkgPath } from "./paths";
+import less from "gulp-less";
+import autoprefixer from "gulp-autoprefixer";
+import run from "./run";
 //删除easyest
 
 export const removeDist = () => {
   return delPath(`${pkgPath}/components/dist`);
 };
 
-export default series(async () => removeDist());
+//打包样式
+export const buildStyle = () => {
+  return src(`${componentPath}/src/**/style/**.less`)
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(dest(`${componentPath}/dist/arim-ui/lib/src`))
+    .pipe(dest(`${componentPath}/dist/arim-ui/es/src`));
+};
+//打包组件
+export const buildComponent = async () => {
+  run("pnpm run build", componentPath);
+};
+
+export default series(
+  async () => removeDist(),
+  parallel(
+    async () => buildStyle(),
+    async () => buildComponent()
+  )
+);
