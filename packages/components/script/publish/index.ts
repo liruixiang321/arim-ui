a66d524566640f223a100caf9d3f871a6dc25d7c
liruixaing <3054809485@qq.com>
2024-08-23 08:21:13 +0800

    chore(release超时时间): 🐳

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -2,6 +2,6 @@ import run from "../build/run";
 import { componentPath } from "../build/paths";
 import { series } from "gulp";
 export const publishComponent = async () => {
-  run("release-it", `${componentPath}/dist/arim-ui`);
+  run("release-it --npm.timeout=1000", `${componentPath}/dist/arim-ui`);
 };
 export default series(async () => publishComponent());
