0a6f3dffcec11cd1e7c7fa19330411e69e49b89f
liruixaing <3054809485@qq.com>
2024-08-26 09:31:08 +0800

    perf(stylelint): 🎈 添加stylelint配置

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,39 +1,38 @@
 <script setup lang="ts">
-	import HelloWorld from './components/HelloWorld.vue'
-	import { Button } from '@arim-ui/components'
-	//   import { Button } from "../../packages/arim-ui/es/src/index";
-	import { type } from '../../../jx-qms-front/src/layouts/default/tabs/types'
+  import HelloWorld from './components/HelloWorld.vue';
+  import { Button } from '@arim-ui/components';
+  //   import { Button } from "../../packages/arim-ui/es/src/index";
 </script>
 
 <template>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
 
-		<ar-button type="primary" />
-		<ar-button />
-		<ar-button />
-		<Button type="primary"></Button>
+    <ar-button type="primary" />
+    <ar-button />
+    <ar-button />
+    <Button type="primary"></Button>
 
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
-		transition: filter 300ms;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.vue:hover {
-		filter: drop-shadow(0 0 2em #42b883aa);
-	}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
